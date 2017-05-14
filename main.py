c298f39eecbff45bc12ce128147cab142684678b
wanglifeng <wanglifeng@yidian-inc.com>
2017-05-14 17:17:58 +0800

    fix bug

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from optparse import OptionParser, make_option
 from fabric.thread_handling import ThreadHandler
 from fabric.io import input_loop, output_loop
 from fabric.context_managers import char_buffered
-
-import termios
-import tty
+from fabric.state import env, env_options, default_channel
 
 import paramiko as ssh
 
@@ -87,63 +85,10 @@ def parse_options():
     opts, args = parser.parse_args()
     return opts
 
-def get_connection(options):
-    client = ssh.SSHClient()
-    tries = 3
-
-    for trie in xrange(0, tries):
-        try:
-            kwargs = dict(
-                hostname=options.host,
-                port=int(options.port),
-                username=options.user,
-                password=options.password,
-                timeout=options.timeout,
-            )
-
-            # Ready to connect
-
-            client.load_system_host_keys()
-            client.set_missing_host_key_policy(ssh.AutoAddPolicy())
-            client.connect(**kwargs)
-            chan = client.get_transport().open_session()
-            chan.settimeout(0.1)
-            chan.input_enabled = True
-            return chan
-
-        except Exception, e:
-            print e
-
-
-def isatty(stream):
-    """Check if a stream is a tty.
-
-    Not all file-like objects implement the `isatty` method.
-    """
-    fn = getattr(stream, 'isatty', None)
-    if fn is None:
-        return False
-    return fn()
-
-
-def char_buffereds(pipe):
-    """
-    Only applies on Unix-based systems; on Windows this is a no-op.
-    """
-    if not isatty(pipe):
-        yield
-    else:
-        old_settings = termios.tcgetattr(pipe)
-        tty.setcbreak(pipe)
-        try:
-            yield
-        finally:
-            termios.tcsetattr(pipe, termios.TCSADRAIN, old_settings)
-
 
 def execute_cmd(options):
-    channel = get_connection(options)
-    timeout = None
+    channel = default_channel()
+    timeout = options.command_timeout
 
     with char_buffered(sys.stdin):
         # Combine stdout and stderr to get around oddball mixing issues
@@ -168,7 +113,7 @@ def execute_cmd(options):
             ThreadHandler('in', input_loop, channel, using_pty)
         )
 
-        channel.sendall(command + "\n")
+        channel.sendall(options.command + "\n")
         channel.sendall('exit\n')
 
         while True:
@@ -202,6 +147,15 @@ def execute_cmd(options):
 def main():
     try:
         options = parse_options()
+
+        for option in env_options:
+            if hasattr(options, option.dest):
+                env[option.dest] = getattr(options, option.dest)
+            else:
+                env[option.dest] = option.default
+
+        env['host_string'] = '%s@%s:%d' % (options.user, options.host, options.port)
+
         execute_cmd(options)
 
     except Exception, e:
